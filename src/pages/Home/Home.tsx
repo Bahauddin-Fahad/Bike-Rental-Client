fc42f8c7eebbdd263e6b4bd6f9ed3bc3d7584618
bahauddin fahad <bahauddin.fahad819@gmail.com>
2024-08-30 20:07:58 +0600

    responsive added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Banner from "./Banner";
 import ContactUs from "./ContactUs";
 import HomeBikes from "./HomeBikes";
@@ -5,9 +6,9 @@ import Testimonial from "./Testimonial";
 import WhyChooseUs from "./WhyChooseUs";
 
 const Home = () => {
-  // useEffect(() => {
-  //   window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  // });
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
 
   return (
     <div className="space-y-10 xl:space-y-[120px] mb-10 xl:mb-[120px]">
